9f075e57df31baafe752f9473103cda8db70a604
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-04-04 11:51:37 +0200

    Add configuration to module

--- modules/wsf/wsf.php
+++ modules/wsf/wsf.php
@@ -16,9 +16,31 @@ class Wsf extends Module
     $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
   }
 
-  public function install()
+   public function install()
   {
-    return (parent::install() && $this->registerhook('displayProductButtons'));
+    return (parent::install() && $this->registerHook('displayProductButtons'));
+  }
+
+  public function getContent()
+  {
+    $this->_postProcess();
+
+    $action = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+    $token = Tools::getAdminTokenLite('AdminModules');
+    $form = '
+      <form action="'. $action .'&token='.$token.'" method="post">
+        <textarea name="SWF_URL" id="" cols="30" rows="10">'. base64_decode(Configuration::get('SWF_URL')) .'</textarea>
+        <input type="submit" value="Valider" name="submitWsf" />
+      </form>
+    ';
+    return $form;
+  }
+
+  protected function _postProcess()
+  {
+    if (Tools::isSubmit('submitWsf')) {
+      Configuration::updateValue('SWF_URL', base64_encode(Tools::getValue('SWF_URL')));
+    }
   }
 
   public function uninstall()
@@ -26,22 +48,15 @@ class Wsf extends Module
     return (parent::uninstall());
   }
 
-  public function getContent()
-  {
-    //$form = $this->renderForm();
-    //return $form;
-  }
-
   public function hookDisplayProductButtons($params)
   {
     $product = $params['product'];
-    //$iframe = str_replace('__SWF_KEYWORD__', $product->name, base64_decode();
-
-    //$this->smarty->assign(
-      //array(
-        //'iframe' => $iframe,
-      //)
-    //);
+    $data = base64_decode(Configuration::get('SWF_URL'));
+    $this->smarty->assign(
+      array(
+        'data' => $data
+      )
+    );
     return $this->display(__FILE__, 'displayProductButtons.tpl');
   }
 }
